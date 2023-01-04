29c5ee6d31ac422a4c097c8238ac20ad6cdad0b0
fatihbatu <77248966+fatihbatu@users.noreply.github.com>
2023-01-04 16:34:21 +0300

    Update connect.php
    Db changes updated in connect.php for initial create action.

--- connect.php
+++ connect.php
@@ -15,19 +15,24 @@ try {
 	$pdo = new PDO("mysql:host=$host;dbname=$db", $root, $root_password);
 	// users table create if not exist with 1 row for deafult actions
 	$pdo->query("CREATE TABLE IF NOT EXISTS `users` (
-	`users_id` int(11) NOT NULL,
-	`users_fname` varchar(50) NOT NULL,
-	`users_lname` varchar(50) NOT NULL,
-	`users_email` varchar(50) NOT NULL,
-	`users_time` timestamp NOT NULL DEFAULT current_timestamp()
-	) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
-	INSERT INTO `users` (`users_id`, `users_fname`, `users_lname`, `users_email`, `users_time`) VALUES
-	(1, 'Fatih', 'Batu', 'me@fatihbatu.com', '2022-12-27 15:49:15');
-	ALTER TABLE `users`
-	ADD PRIMARY KEY (`users_id`);
-	ALTER TABLE `users`
-	MODIFY `users_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-	COMMIT;");
+  `users_id` int(11) NOT NULL,
+  `users_fname` varchar(50) NOT NULL,
+  `users_lname` varchar(50) NOT NULL,
+  `users_email` varchar(50) NOT NULL,
+  `users_time` timestamp NOT NULL DEFAULT current_timestamp(),
+  `users_state` tinyint(1) NOT NULL DEFAULT 1,
+  `users_gender` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+INSERT INTO `users` (`users_id`, `users_fname`, `users_lname`, `users_email`, `users_time`, `users_state`, `users_gender`) VALUES
+(1, 'Fatih', 'Batu', 'me@fatihbatu.com', '2022-12-27 12:49:15', 1, '');
+
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`users_id`);
+
+ALTER TABLE `users`
+  MODIFY `users_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;");
 
 	$pdo->query("CREATE TABLE IF NOT EXISTS `admins` (
   `admin_id` int(11) NOT NULL,
