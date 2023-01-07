1cc03da09a391281ea3092aef8abfe80b2552f3f
fatihbatu <77248966+fatihbatu@users.noreply.github.com>
2023-01-07 23:00:59 +0300

    Update header.php

--- header.php
+++ header.php
@@ -62,7 +62,7 @@ if ($count==0) {
         <div class="d-flex align-items-center justify-content-between">
             <a href="index.php" class="logo d-flex align-items-center">
                 <img src="assets/img/logo.png" alt="">
-                <span class="d-none d-lg-block">FmB</span>
+                <span class="d-none d-lg-block">Mail List Demo</span>
             </a>
             <i class="bi bi-list toggle-sidebar-btn"></i>
         </div><!-- End Logo -->
